ed916ecd3cd6c6eac467c1b9849550cacab337e1
Pranjal095 <pranjal.prajapati35@gmail.com>
2025-11-23 22:54:36 +0530

    fix: update tokens.h

--- compiler/tokens.h
+++ compiler/tokens.h
@@ -44,16 +44,21 @@ extern int yydebug;
     T_FLOAT_LITERAL = 282,         /* T_FLOAT_LITERAL  */
     T_STRING_LITERAL = 283,        /* T_STRING_LITERAL  */
     T_ID = 284,                    /* T_ID  */
-    T_AND = 285,                   /* T_AND  */
-    T_OR = 286,                    /* T_OR  */
-    T_EQ = 287,                    /* T_EQ  */
-    T_NEQ = 288,                   /* T_NEQ  */
-    T_LT = 289,                    /* T_LT  */
-    T_GT = 290,                    /* T_GT  */
-    T_LTE = 291,                   /* T_LTE  */
-    T_GTE = 292,                   /* T_GTE  */
-    T_ASSIGN = 293,                /* T_ASSIGN  */
-    T_RANGE = 294                  /* T_RANGE  */
+    T_PLUS = 285,                  /* T_PLUS */
+    T_MINUS = 286,                 /* T_MINUS */
+    T_MUL = 287,                   /* T_MUL */
+    T_DIV = 288,                   /* T_DIV */
+    T_MOD = 289,                   /* T_MOD */
+    T_AND = 290,                   /* T_AND  */
+    T_OR = 291,                    /* T_OR  */
+    T_EQ = 292,                    /* T_EQ  */
+    T_NEQ = 293,                   /* T_NEQ  */
+    T_LT = 294,                    /* T_LT  */
+    T_GT = 295,                    /* T_GT  */
+    T_LTE = 296,                   /* T_LTE  */
+    T_GTE = 297,                   /* T_GTE  */
+    T_ASSIGN = 298,                /* T_ASSIGN  */
+    T_RANGE = 299                  /* T_RANGE  */
   };
   typedef enum yytokentype yytoken_kind_t;
 #endif
